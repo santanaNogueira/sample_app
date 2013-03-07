f56691d032550f1419f2825c01186777ae1104ef
satana <jruisantana@gmail.com>
2013-03-07 00:07:17 +0000

    add title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,13 +7,17 @@ describe "Static pages" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
-
-			
+	
 		end
 
 		it "should have the right title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => 'Twitter App | Home')
+			page.should have_selector('title', :text => 'Twitter App')
+		end
+
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
 
