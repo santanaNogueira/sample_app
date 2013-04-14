dfc19749dc705d6d4eb6c4aa2ce05aa5290e693b
satana <jruisantana@gmail.com>
2013-04-14 02:08:47 +0100

    sera desta?

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,9 @@ SampleApp::Application.routes.draw do
 
   match 'send_email', to: 'conta#send_email'
 
-  match '/help',     to: 'static_pages#help'
-  match '/contact',  to: 'static_pages#contact'
-  match '/about',    to: 'static_pages#about'
+  # match '/help',     to: 'static_pages#help'
+  # match '/contact',  to: 'static_pages#contact'
+  # match '/about',    to: 'static_pages#about'
 
 
 
